573ec5103c9c7ee252940788b98bd18304e07fd1
Ulises Nuñez <gem2ulysses@gmail.com>
2025-05-30 01:13:43 -0600

    🦄 refactor(Module): Main
    Main Module configurated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env'
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (
+        configService: ConfigService
+      ): Promise<MongooseModuleOptions> => ({
+        uri: configService.get<string>('MONGO_INITDB_CONNECTION_URI'),
+        user: configService.get<string>('MONGO_INITDB_ROOT_USERNAME'),
+        pass: configService.get<string>('MONGO_INITDB_ROOT_PASSWORD'),
+        dbName: configService.get<string>('MONGO_INITDB_NAME'),
+      }),
+      inject: [ConfigService]
+    })
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true
+    })
+  );
+  app.enableCors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    allowedHeaders: 'Content-Type, Authorization'
+  })
 }
 bootstrap();
