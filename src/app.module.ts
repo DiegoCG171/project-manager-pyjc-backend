d04a546c8e9c7982055fa7bd36c64002acc651c3
Helena <velazquez.helena.2@gmail.com>
2025-06-09 15:36:45 -0600

    appmodule-fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,45 +3,36 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import { RolModule } from './rol/rol.module';
-=======
-=======
+
 import { SeedModule } from './seed/seed.module';
->>>>>>> 5ba24cb3b2e270845fe49c99ddb0ef53f9e69f10
+
 import { AreaModule } from './area/area.module';
->>>>>>> 4427f3fed7993584290ec30dafff2aaeab35e47e
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env'
+      envFilePath: '.env',
     }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (
-        configService: ConfigService
+        configService: ConfigService,
       ): Promise<MongooseModuleOptions> => ({
         uri: configService.get<string>('MONGO_INITDB_CONNECTION_URI'),
         user: configService.get<string>('MONGO_INITDB_ROOT_USERNAME'),
         pass: configService.get<string>('MONGO_INITDB_ROOT_PASSWORD'),
         dbName: configService.get<string>('MONGO_INITDB_NAME'),
       }),
-      inject: [ConfigService]
+      inject: [ConfigService],
     }),
     AuthModule,
     UserModule,
-<<<<<<< HEAD
-<<<<<<< HEAD
-    RolModule
-=======
-=======
+    RolModule,
     SeedModule,
->>>>>>> 5ba24cb3b2e270845fe49c99ddb0ef53f9e69f10
-    AreaModule
->>>>>>> 4427f3fed7993584290ec30dafff2aaeab35e47e
+    AreaModule,
   ],
   controllers: [],
   providers: [],
